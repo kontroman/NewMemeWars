62beb1b49eb1d2366a9b3ac27667d357015c6b82
Kontroman <ckontro@gmail.com>
2022-02-09 21:04:47 +0600

    +GameTimer

---
+++ Assets/Scripts/UI/GameCanvas.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameCanvas : MonoBehaviour
+{
+    public static GameCanvas Instance { get; private set; }
+
+    public Text roundTimeText;
+
+    private void Awake()
+    {
+        if (Instance != null) return;
+        else Instance = this;
+    }
+
+    public void UpdateRoundTime(float time)
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        roundTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
