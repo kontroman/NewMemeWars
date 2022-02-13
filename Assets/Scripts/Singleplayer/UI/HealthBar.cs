48653b554822dbdf22cde826d3de0711ba6a0020
Kontroman <ckontro@gmail.com>
2022-02-13 15:56:36 +0600

    Assault prefab

---
+++ Assets/Scripts/Singleplayer/UI/HealthBar.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Image healthBarImage;
+
+    float actualValue = 1f;
+    float startValue = 1f;
+    float displayValue = 1f;
+    float timer = 0f;
+
+    private void Awake()
+    {
+        healthBarImage = GetComponent<Image>();
+        healthBarImage.fillAmount = 1;
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2f);
+        HealthChangerHandler(80);
+        yield return new WaitForSeconds(2f);
+        HealthChangerHandler(23);
+
+    }
+
+    private void OnEnable()
+    {
+        //HealthComponent.OnHealthChange += HealthChangerHandler;
+    }
+
+    private void OnDisable()
+    {
+        //HealthComponent.OnHealthChange -= HealthChangerHandler;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        displayValue = Mathf.Lerp(startValue, actualValue, timer);
+        healthBarImage.fillAmount = displayValue;
+    }
+
+    private void HealthChangerHandler(int _newHealth)
+    {
+        actualValue = _newHealth / 100f;
+        startValue = healthBarImage.fillAmount;
+        timer = 0f;
+    }
+}
